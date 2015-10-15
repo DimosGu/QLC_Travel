29a20c4047ed8352f160ac571da29631f59dcbb3
斌斌斌 <1272917376@qq.com>
2015-10-15 11:43:22 +0800

    第一次提交工程

---
+++ QLC_Travel/Source/Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLC_Travel
+//
+//  Created by 斌斌斌 on 15/10/15.
+//  Copyright (c) 2015年 QLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
